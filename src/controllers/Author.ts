edd23377f79157f208049019a6eaa6de9ea954bf
kiqueangulo <kiqueangulo@gmail.com>
2022-10-12 00:32:27 -0400

    Set up the functionality for each author controller and export them

--- src/controllers/Author.ts
+++ src/controllers/Author.ts
@@ -3,12 +3,56 @@ import mongoose from 'mongoose';
 
 import Author from '../models/Author';
 
-const createAuthor = (req: Request, res: Response, next: NextFunction) => {};
+const createAuthor = (req: Request, res: Response, next: NextFunction) => {
+    const { name } = req.body;
 
-const readAuthor = (req: Request, res: Response, next: NextFunction) => {};
+    const author = new Author({ _id: new mongoose.Types.ObjectId(), name: name });
 
-const readAllAuthor = (req: Request, res: Response, next: NextFunction) => {};
+    return author
+        .save()
+        .then((author) => res.status(201).json({ author }))
+        .catch((error) => res.status(500).json({ error }));
+};
 
-const updateAuthor = (req: Request, res: Response, next: NextFunction) => {};
+const readAuthor = (req: Request, res: Response, next: NextFunction) => {
+    const authorId = req.params.authorId;
 
-const deleteAuthor = (req: Request, res: Response, next: NextFunction) => {};
+    return Author.findById(authorId)
+        .then((author) => (author ? res.status(200).json({ author }) : res.status(404).json({ message: 'Not found.' })))
+        .catch((error) => res.status(500).json({ error }));
+};
+
+const readAllAuthors = (req: Request, res: Response, next: NextFunction) => {
+    return Author.find()
+        .then((authors) => res.status(200).json({ authors }))
+        .catch((error) => res.status(500).json({ error }));
+};
+
+const updateAuthor = (req: Request, res: Response, next: NextFunction) => {
+    const authorId = req.params.authorId;
+
+    return Author.findById(authorId)
+        .then((author) => {
+            if (author) {
+                author.set(req.body);
+
+                return author
+                    .save()
+                    .then((author) => res.status(201).json({ author }))
+                    .catch((error) => res.status(500).json({ error }));
+            } else {
+                res.status(404).json({ message: 'Not found.' });
+            }
+        })
+        .catch((error) => res.status(500).json({ error }));
+};
+
+const deleteAuthor = (req: Request, res: Response, next: NextFunction) => {
+    const authorId = req.params.authorId;
+
+    return Author.findByIdAndDelete(authorId)
+        .then((author) => (author ? res.status(201).json({ message: 'Deleted' }) : res.status(404).json({ message: 'Not found.' })))
+        .catch((error) => res.status(500).json({ error }));
+};
+
+export default { createAuthor, readAuthor, readAllAuthors, updateAuthor, deleteAuthor };
