7414d9161e5e38f3930bf9df47d8fa771a955ee7
kiqueangulo <kiqueangulo@gmail.com>
2022-10-12 00:13:34 -0400

    Set the controllers needed for author

---
+++ src/controllers/Author.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from 'express';
+import mongoose from 'mongoose';
+
+import Author from '../models/Author';
+
+const createAuthor = (req: Request, res: Response, next: NextFunction) => {};
+
+const readAuthor = (req: Request, res: Response, next: NextFunction) => {};
+
+const readAllAuthor = (req: Request, res: Response, next: NextFunction) => {};
+
+const updateAuthor = (req: Request, res: Response, next: NextFunction) => {};
+
+const deleteAuthor = (req: Request, res: Response, next: NextFunction) => {};
