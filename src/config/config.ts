e038dc12679c80084cd03c2a5e4d696b5d0a5eb4
kiqueangulo <kiqueangulo@gmail.com>
2022-10-12 02:51:15 -0400

    Refactor mongo url from config file

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,9 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
-const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@restful-api-typescript.zfpupvu.mongodb.net/another-api`;
+const MONGO_URL = process.env.MONGO_URL || '';
 
 const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 1337;
 
