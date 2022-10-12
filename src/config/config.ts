a5504e0597daef8f74d92d6a9056f326dd15c503
kiqueangulo <kiqueangulo@gmail.com>
2022-10-11 22:14:58 -0400

    Set up and export env variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,11 @@
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@restful-api-typescript.zfpupvu.mongodb.net/another-api?retryWrites=true&w=majority`;
+
+const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 1337;
+
+export const config = { mongo: { url: MONGO_URL }, server: { port: SERVER_PORT } };
