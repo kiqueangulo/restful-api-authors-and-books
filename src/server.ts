ca0b8f900e02964f6281988d5be25142d7c20cca
kiqueangulo <kiqueangulo@gmail.com>
2022-10-11 23:12:11 -0400

    Create the instance of express and start the server only after the database is connected

--- src/server.ts
+++ src/server.ts
@@ -7,11 +7,29 @@ import { config } from './config/config';
 
 const router = express();
 
+const startServer = () => {
+    router.use((req, res, next) => {
+        /* Log the request */
+        Logging.info(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+        res.on('finish', () => {
+            /* Log the response */
+            Logging.info(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${res.statusCode}]`);
+        });
+
+        next();
+    });
+
+    router.use(express.urlencoded({ extended: true }));
+    router.use(express.json());
+};
+
 /* Connect to Mongo */
 mongoose
     .connect(config.mongo.url, { retryWrites: true, w: 'majority' })
     .then(() => {
         Logging.info('Connected to database.');
+        startServer(); /* Only start the server if Mongo connects */
     })
     .catch((error) => {
         Logging.error('Unable to connect: ');
