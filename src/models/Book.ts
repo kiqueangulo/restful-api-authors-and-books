2b8836ed06de7e8270ff989895bc74755993ec3e
kiqueangulo <kiqueangulo@gmail.com>
2022-10-12 01:13:41 -0400

    Create book models

---
+++ src/models/Book.ts
@@ -0,0 +1,21 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IBook {
+    title: string;
+    author: string;
+}
+
+export interface IBookModel extends IBook, Document {}
+
+const BookSchema = new Schema(
+    {
+        title: { type: String, require: true },
+        author: { type: Schema.Types.ObjectId, require: true, ref: 'Author' }
+    },
+    {
+        timestamps: true,
+        versionKey: true
+    }
+);
+
+export default mongoose.model<IBookModel>('Book', BookSchema);
