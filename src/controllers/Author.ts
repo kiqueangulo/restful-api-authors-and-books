c61c618bd7166e9372f42eede025ede73d15eab3
kiqueangulo <kiqueangulo@gmail.com>
2022-10-12 01:36:27 -0400

    Create book controllers

--- src/controllers/Author.ts
+++ src/controllers/Author.ts
@@ -1,9 +1,9 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import mongoose from 'mongoose';
 
 import Author from '../models/Author';
 
-const createAuthor = (req: Request, res: Response, next: NextFunction) => {
+const createAuthor = (req: Request, res: Response) => {
     const { name } = req.body;
 
     const author = new Author({ _id: new mongoose.Types.ObjectId(), name: name });
@@ -14,7 +14,7 @@ const createAuthor = (req: Request, res: Response, next: NextFunction) => {
         .catch((error) => res.status(500).json({ error }));
 };
 
-const readAuthor = (req: Request, res: Response, next: NextFunction) => {
+const readAuthor = (req: Request, res: Response) => {
     const authorId = req.params.authorId;
 
     return Author.findById(authorId)
@@ -22,13 +22,13 @@ const readAuthor = (req: Request, res: Response, next: NextFunction) => {
         .catch((error) => res.status(500).json({ error }));
 };
 
-const readAllAuthors = (req: Request, res: Response, next: NextFunction) => {
+const readAllAuthors = (req: Request, res: Response) => {
     return Author.find()
         .then((authors) => res.status(200).json({ authors }))
         .catch((error) => res.status(500).json({ error }));
 };
 
-const updateAuthor = (req: Request, res: Response, next: NextFunction) => {
+const updateAuthor = (req: Request, res: Response) => {
     const authorId = req.params.authorId;
 
     return Author.findById(authorId)
@@ -47,7 +47,7 @@ const updateAuthor = (req: Request, res: Response, next: NextFunction) => {
         .catch((error) => res.status(500).json({ error }));
 };
 
-const deleteAuthor = (req: Request, res: Response, next: NextFunction) => {
+const deleteAuthor = (req: Request, res: Response) => {
     const authorId = req.params.authorId;
 
     return Author.findByIdAndDelete(authorId)
