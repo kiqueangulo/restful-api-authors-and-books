04821c64c9cc5e26b3c1b30bcd900d92681b3a4a
kiqueangulo <kiqueangulo@gmail.com>
2022-10-11 23:23:01 -0400

    Set up the rules of the api
    The first header says the request can come from anywhere if I want to restrict it I can add a list of IP addresses The second header says what headers are allowed to be used and finally if an OPTIONS request is sent I can return all the options that can be used inside this api

--- src/server.ts
+++ src/server.ts
@@ -22,6 +22,20 @@ const startServer = () => {
 
     router.use(express.urlencoded({ extended: true }));
     router.use(express.json());
+
+    /* Rule of API */
+    router.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+        if (req.method == 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+
+            return res.status(200).json({});
+        }
+
+        next();
+    });
 };
 
 /* Connect to Mongo */
